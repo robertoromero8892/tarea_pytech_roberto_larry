28011d06d152997b7eae70a1e26d4ab1ce25343a
robertoromero8892 <robertoromero8892@gmail.com>
2015-04-20 08:53:08 -0430

    commit de suite de pruebas con exceptions

--- tarea2/SuitedePruebas.py
+++ tarea2/SuitedePruebas.py
@@ -1,19 +1,38 @@
-'''Suite de casos de pruebas
+'''Suite de casos de pruebas para la funcion CalcularPrecio() 
+Creado el 19/04/2015
+Equipo:Pytech
 Autores:
 Roberto Romero10-10642
 Larry Perez 10-10547
 '''
+
 import unittest
 from datetime import datetime
 from tarea2 import Tarifa, calcularPrecio 
 
 class Test(unittest.TestCase):
     
-        def testprueba(self):
-            tari = Tarifa (1,1)
+        def testTarifaCero(self):
+            tari = Tarifa (0,0)
             tiempo = [datetime(year=2015,month=1,day=1),datetime(year=2015,month=1,day=3)]
             resultado = calcularPrecio(tari,tiempo)
-            assert resultado == 48.00    
- 
+            assert resultado == 0.00    
+        
+        def testTarifaMax(self):
+            tari = Tarifa (4294967295,4294967295)
+            tiempo = [datetime(year=2015,month=1,day=1),datetime(year=2015,month=1,day=8)]
+            calcularPrecio(tari,tiempo)
+            
+        def testMaxDias(self):
+            tari = Tarifa (1,1)
+            tiempo = [datetime(year=2015,month=1,day=1),datetime(year=2015,month=1,day=8,second=1)]
+            self.assertRaises(Exception,calcularPrecio,tari,tiempo) 
+
+        def testMinTiempo(self):
+            tari = Tarifa (1,1)
+            tiempo = [datetime(year=2015,month=1,day=1),datetime(year=2015,month=1,day=1,minute=14,second=59)]
+            self.assertRaises(Exception,calcularPrecio,tari,tiempo) 
+            
+        
 if __name__ == "__main__":
     unittest.main()      
